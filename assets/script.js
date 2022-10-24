d0aa4ab53e7163069e89f83d1a6b7675257fa082
bill6653 <billcrs512@gmail.com>
2022-10-24 18:13:16 -0500

    added more moment.js,added HTML search results, fetched UV info for color display of UV index

--- assets/script.js
+++ assets/script.js
@@ -35,4 +35,45 @@ var getCurrentConditions = (event) => {
         let currentTimeZoneOffsetHours = currentTimeZoneOffset / 60 / 60;
         let currentMoment = moment.unix(currentTimeUTC).utc().utcOffset(currentTimeZoneOffsetHours);
         
+        renderCities();
+
+        getFiveDayForecast(event);
+
+        $('#header-text').text(response.name);
+
+        let currentWeatherHTML = `
+        <h3>${response.name} ${currentMoment.format("(MM/DD/YY")}<img src=${currentWeatherIcon}"></h3>
+        <ul class="list-unstyled">
+        <li>Temperature: ${response.main.temp}$#8457;</li>
+        <li>Humidity: ${response.main.humidity}%</li>
+        <li id="uvIndex">UV Index:</li>
+        </ul>`;
+
+        $('#current-weather').html(currentWeatherHTML);
+
+        let latitude = response.coord.lat;
+        let longitude = response.coord.lon;
+        let uvQueryURL = "api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "$lon=" +longitude + "&APPID=" + owmAPI;
+
+        uvQueryURL = "https://cors-anywhere.herokuapp.com/" + uvQueryURL;
+
+        fetch(uvQueryURL)
+        .then(handleErrors)
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+            let uvIndex = response.value;
+            $('#uvIndex').html(`UV Index: <span id="uvVal"> ${uvIndex}</span>`);
+            if (uvIndex>=0 && uvIndex<3){
+                $('#uvVal').attr("class", "uv-favorable");
+            } else if (uvIndex>=3 && uvIndex<8){
+                $('#uvVal').attr("class", "uv-moderate");
+            } else if (uvIndex>=8){
+                $('#uvVal').attr("class", "uv-severe");
+            }
+        });
+    })
+}
+
         
