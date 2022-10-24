e45f8b1953d23b55c828d9370206427cf9c86360
bill6653 <billcrs512@gmail.com>
2022-10-24 17:07:14 -0500

    set global variables,added fetch API function, created moment.js elements

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+
+var owmAPI = "0a79fcd39d8f75343c924bb6535d87f4";
+var currentCity = "";
+var lastCity = "";
+
+
+var handleErrors = (response) => {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
+}
+
+
+var getCurrentConditions = (event) => {
+    
+    let city = $('#search-city').val();
+    currentCity= $('#search-city').val();
+    
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + owmAPI;
+    fetch(queryURL)
+    .then(handleErrors)
+    .then((response) => {
+        return response.json();
+    })
+    .then((response) => {
+        
+        saveCity(city);
+        $('#search-error').text("");
+        
+        let currentWeatherIcon="https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+        
+        let currentTimeUTC = response.dt;
+        let currentTimeZoneOffset = response.timezone;
+        let currentTimeZoneOffsetHours = currentTimeZoneOffset / 60 / 60;
+        let currentMoment = moment.unix(currentTimeUTC).utc().utcOffset(currentTimeZoneOffsetHours);
+        
+        
