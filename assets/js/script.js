c78d095ae7fb534d57c2406b93c943cf703dd1f4
bill6653 <billcrs512@gmail.com>
2023-02-26 22:09:12 -0600

    created display parameters in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,3 +43,39 @@ function currentWeather(city) {
         var weatherIcon= response.weather[0].icon;
         var iconURL = "https://openweathermap.org/img/wn/"+ weatherIcon +"@2x.png";
         var date = new Date(response.dt*1000).toLocaleDateString();
+
+        //parse the response for name of city
+        $(currentCity).html(response.name +"("+date+")" + "<img src="+iconURL+">");
+        
+        // Convert the temp to fahrenheit
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        $(currentTemperature).html((tempF).toFixed(2) + "&#8457");
+        // Display the Humidity
+        $(currentHumidity).html(response.main.humidity + "%");
+        //Display Wind speed and convert to MPH
+        var ws=response.wind.speed;
+        var windsMPH=(ws*2.237).toFixed(1);
+
+        $(currentWindSpeed).html(windsMPH+"MPH");
+        forecast(response.id);
+        if(response.cod==200){
+            sCity=JSON.parse(localStorage.getItem("city-name"));
+            console.log(sCity);
+            if (sCity==null){
+                sCity=[];
+                sCity.push(city.toUpperCase()
+                );
+                localStorage.setItem("city-name",JSON.stringify(sCity));
+                addToList(city);
+            }
+            else {
+                if(find(city) > 0) {
+                    sCity.push(city.toUpperCase());
+                    localStorage.setItem("city-name",JSON.stringify(sCity));
+                    addToList(city);
+                }
+            }
+        }
+
+    });
+}
