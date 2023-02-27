5c508b176c9ab75e479c6bd8991d6618ecd7ad0c
bill6653 <billcrs512@gmail.com>
2023-02-26 22:27:44 -0600

    functions for search history, render and handlers

--- assets/js/script.js
+++ assets/js/script.js
@@ -104,3 +104,47 @@ function forecast(cityid){
         
     });
 }
+// adds the passed city on the search history
+function addToList(c){
+    var listEl= $("<li>"+c.toUpperCase()+"</li>");
+    $(listEl).attr("class","list-group-item");
+    $(listEl).attr("data-value",c.toUpperCase());
+    $(".list-group").append(listEl);
+}
+// display the past search when the list group item is clicked in search history
+function invokePastSearch(event){
+    var liEl=event.target;
+    if (event.target.matches("li")){
+        city=liEl.textContent.trim();
+        currentWeather(city);
+    }
+
+}
+
+// render function
+function loadlastCity(){
+    $("ul").empty();
+    var sCity = JSON.parse(localStorage.getItem("city-name"));
+    if(sCity!==null){
+        sCity=JSON.parse(localStorage.getItem("city-name"));
+        for(i=0; i<sCity.length;i++){
+            addToList(sCity[i]);
+        }
+        city=sCity[i-1];
+        currentWeather(city);
+    }
+
+}
+//Clear the search history from the page
+function clearHistory(event){
+    event.preventDefault();
+    sCity=[];
+    localStorage.removeItem("city-name");
+    document.location.reload();
+
+}
+//Click Handlers
+$("#search-button").on("click",displayWeather);
+$(document).on("click",invokePastSearch);
+$(window).on("load", loadlastCity);
+$("#clear-history").on("click",clearHistory);
